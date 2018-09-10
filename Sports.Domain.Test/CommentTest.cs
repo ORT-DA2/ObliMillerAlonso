afb16257e908847e603a40ea6cdf29739911ed3b
itaimiller <itaimiller@hotmail.com>
2018-09-10 19:30:20 +0200

    SetUp() - [RED] - Comment class is not created.

---
+++ Sports.Domain.Test/CommentTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Sports.Domain.Test
+{
+    public class CommentTest
+    {
+        Comment comment;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            comment = new Comment();
+        }
+    }
+}
