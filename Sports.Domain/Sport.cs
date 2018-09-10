79da38b32f6a4f7a5d6bd674ea7a8731afc23dd4
itaimiller <itaimiller@hotmail.com>
2018-09-10 21:26:18 +0200

    InvalidSportName() - [GREEN] - test is ok.

--- Sports.Domain/Sport.cs
+++ Sports.Domain/Sport.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Sports.Exceptions;
 
 namespace Sports.Domain
 {
     public class Sport
     {
         public string Name { get; set; }
+
+        public void IsValidSportName()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                throw new InvalidSportDataException("Invalid Name");
+        }
     }
 }
---
+++ Sports.Exceptions/InvalidSportDataException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Sports.Exceptions
+{
+    [Serializable]
+    public class InvalidSportDataException : Exception
+    {
+        public InvalidSportDataException()
+        {
+        }
+
+        public InvalidSportDataException(string message) : base(message)
+        {
+        }
+
+        public InvalidSportDataException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidSportDataException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
