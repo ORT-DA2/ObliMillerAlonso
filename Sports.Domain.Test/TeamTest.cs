7e6ae7da6e025cdd853c68aa17a2d4eee5d5212b
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2018-09-10 12:25:16 -0300

    AddPictureFromPath [RED] - Method add picture from path not created

--- Sports.Domain.Test/TeamTest.cs
+++ Sports.Domain.Test/TeamTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Sports.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
 
 namespace Sports.Domain.Test
 {
@@ -44,5 +46,18 @@ namespace Sports.Domain.Test
             team.IsValid();
             Assert.AreEqual<string>(team.Name, "Test Team");
         }
+
+        [TestMethod]
+        public void AddPictureFromPath()
+        {
+            string picturePath = "C: /Users/Rafael/Documents/Diseno2/MillerAlonso/Sports.Domain.Test/gun.png";
+            team.AddPictureFromPath();
+            byte[] file = File.ReadAllBytes(picturePath);
+            Assert.IsTrue(Enumerable.SequenceEqual(file, team.Picture));
+        }
+
+        //invalidImagePath
+
+        //invalidImageFile
     }
 }
