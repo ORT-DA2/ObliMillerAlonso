837e5039fe257f8dfda4ce70cfd4e1b1fae0faab
itaimiller <itaimiller@hotmail.com>
2018-09-10 15:53:31 +0200

    SetUp() [RED] - class User is not created.

---
+++ Sports.Domain.Test/UserTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sports.Domain.Test
+{
+    [TestClass]
+    public class UserTest
+    {
+        User user;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            user = new User();
+        }
+       
+       
+    }
+}
