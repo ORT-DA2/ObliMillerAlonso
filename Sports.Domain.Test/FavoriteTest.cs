200d9bb1750f297d408f30e73d01311fd14aceb9
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2018-09-10 16:28:21 -0300

    NewFavorite [RED] - class favorite not created

---
+++ Sports.Domain.Test/FavoriteTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sports.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sports.Exceptions;
+
+namespace Sports.Domain.Test
+{
+    public class FavoriteTest
+    {
+        Favorite favorite;
+        [TestInitialize]
+        public void SetUp()
+        {
+            User user = new User();
+            Team team = new Team();
+            favorite = new Favorite()
+            {
+               User = user,
+               Team = team
+
+            };
+        }
+
+        [TestMethod]
+        public void NewFavorite()
+        {
+            Assert.IsNotNull(favorite);
+        }
+    }
+}
