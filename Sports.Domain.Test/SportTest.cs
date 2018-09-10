c3ed5796d1687144185a01d4715afe8b31ca53cb
itaimiller <itaimiller@hotmail.com>
2018-09-10 21:17:44 +0200

    SetUp() - [RED] - class Sport is not created.

---
+++ Sports.Domain.Test/SportTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Sports.Domain.Test
+{
+    [TestClass]
+    public class SportTest
+    {
+        Sport sport;
+
+        [TestMethod]
+        public void SetUp()
+        {
+            sport = new Sport()
+            {
+                Name = "Test sport"
+            };
+        }
+    }
+}
