80ab49b992f35144f0bbe8ca5a197fc4389891fa
itaimiller <itaimiller@hotmail.com>
2018-09-10 20:20:08 +0200

    added Id property to class Comment

--- Sports.Domain/Comment.cs
+++ Sports.Domain/Comment.cs
@@ -7,6 +7,7 @@ namespace Sports.Domain
 {
     public class Comment
     {
+        public int Id { get; private set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
         private User user;
