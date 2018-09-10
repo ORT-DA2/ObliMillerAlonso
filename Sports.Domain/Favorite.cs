eb78be09e5d319760ca4925794e1d965fc0e5586
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2018-09-10 16:34:53 -0300

    NewFavorite [GREEN] - favorite is created succesfully

---
+++ Sports.Domain/Favorite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sports.Domain
+{
+    public class Favorite
+    {
+        public int Id { get; private set; }
+        public User User { get; set; }
+        public Team Team { get; set; }
+
+    }
+}
