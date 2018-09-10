3c43535c0c08f955440012de54df07de2da013f5
itaimiller <itaimiller@hotmail.com>
2018-09-10 21:18:52 +0200

    SetUp() - [GREEN] - test is ok.

---
+++ Sports.Domain/Sport.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sports.Domain
+{
+    public class Sport
+    {
+        public string Name { get; set; }
+    }
+}
