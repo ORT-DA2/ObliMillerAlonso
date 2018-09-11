ea6f7f3a9c27d94a23ce5754cdb96b9b9a56b19a
itaimiller <itaimiller@hotmail.com>
2018-09-11 17:24:46 +0200

    AddUser() - [RED] - AddUser() is not implemented in class UserLogic.

---
+++ Sports.Logic.Test/UserLogicTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sports.Domain;
+
+
+namespace Sports.Logic.Test
+{
+    [TestClass]
+    public class UserLogicTest
+    {
+        [TestMethod]
+        public void AddUser()
+        {
+            User user = new User()
+            {
+                FirstName = "Itai",
+                LastName = "Miller",
+                Email = "itaimiller@gmail.com",
+                UserName = "iMiller",
+                Password = "root"
+            };
+            user.Id = UserLogic.AddUser(user);
+            Assert.IsNotNull(UserLogic.GetUserById(user.Id));
+        }
+    }
+}
