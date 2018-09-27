5f3399d9ceb00ac3c1edbf86d56f3095307c1073
itaimiller1 <itaimiller@hotmail.com>
2018-09-27 17:06:19 -0300

    DisconnectedDatabase() - [RED] - test is not ok.

---
+++ Sports.Repository.Test/RepositoryBaseTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Sports.Repository.Context;
+using Sports.Repository.Interface;
+using Sports.Repository;
+using Microsoft.EntityFrameworkCore;
+using Sports.Logic;
+using Sports.Repository.Exceptions;
+using Sports.Logic.Interface;
+
+namespace Sports.Repository.Test
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class RepositoryBaseTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDatabaseAccessException))]
+        public void DisconnectedDatabase()
+        {
+            var options = new DbContextOptionsBuilder<RepositoryContext>().Options;
+            RepositoryContext repository = new RepositoryContext(options);
+            IRepositoryUnitOfWork unit = new RepositoryUnitOfWork(repository);
+            ITeamRepository teamRepository = unit.Team;
+            teamRepository.Save();
+        }
+    }
+}
