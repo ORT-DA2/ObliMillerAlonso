6f3d07814682d21d417d64aa9a07382c51f43bbd
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2018-10-01 12:13:01 -0300

    projects for web api created

---
+++ Sports.WebAPI.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sports.WebAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
