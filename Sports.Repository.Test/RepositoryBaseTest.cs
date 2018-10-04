bfb9fdb3deb992897a8bda6a0d70be171dc820f7
Rafael Alonso <pepe1615@hotmail.com>
2018-10-04 16:21:21 -0300

    AddTeamSqlError [RED] - dbException not caught

--- Sports.Repository.Test/RepositoryBaseTest.cs
+++ Sports.Repository.Test/RepositoryBaseTest.cs
@@ -37,32 +37,46 @@ namespace Sports.Repository.Test
         }
 
         [TestMethod]
-        [ExpectedException(typeof(DisconnectedDatabaseException))]
-        public void DisconnectedDatabaseSave()
+        [ExpectedException(typeof(UnknownDatabaseException))]
+        public void UnknownErrorSave()
         {
             teamRepository.Save();
         }
 
         [TestMethod]
-        [ExpectedException(typeof(DisconnectedDatabaseException))]
-        public void AddTeam()
+        [ExpectedException(typeof(UnknownDatabaseException))]
+        public void UnknownErrorAddTeam()
         {
             teamRepository.Create(team);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(DisconnectedDatabaseException))]
-        public void DeleteTeam()
+        [ExpectedException(typeof(UnknownDatabaseException))]
+        public void UnknownErrorDeleteTeam()
         {
             teamRepository.Delete(team);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(DisconnectedDatabaseException))]
-        public void UpdateTeam()
+        [ExpectedException(typeof(UnknownDatabaseException))]
+        public void UnknownErrorUpdateTeam()
         {
             teamRepository.Update(team);
         }
-        
+
+        [TestMethod]
+        [ExpectedException(typeof(DisconnectedDatabaseException))]
+        public void AddTeamSqlError()
+        {
+            var options = new DbContextOptionsBuilder<RepositoryContext>().UseSqlServer("Server=fakeServer;Database=fakeDb").Options;
+            RepositoryContext fakeRepository = new RepositoryContext(options);
+            IRepositoryUnitOfWork newUnit = new RepositoryUnitOfWork(fakeRepository);
+            ITeamRepository fakeTeamRepository = newUnit.Team;
+            team = new Team()
+            {
+                Name = "Team"
+            };
+            fakeTeamRepository.Create(team);
+        }
     }
 }
